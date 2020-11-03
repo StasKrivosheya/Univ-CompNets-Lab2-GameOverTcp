feb4127dd26ee3d04753ac237a0b74a2d6a35094
Stas Krivosheya <stask49@gmail.com>
2020-11-03 23:17:18 +0200

    Lobby design finished

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; set; }
+
+    public GameObject mainMenu;
+    public GameObject connectMenu;
+    public GameObject serverMenu;
+
+    private void Start()
+    {
+        Instance = this;
+
+        serverMenu.SetActive(false);
+        connectMenu.SetActive(false);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ConnectButton()
+    {
+        mainMenu.SetActive(false);
+        connectMenu.SetActive(true);
+    }
+    public void HostButton()
+    {
+        mainMenu.SetActive(false);
+        serverMenu.SetActive(true);
+
+
+    }
+
+    public void ConnectToServerButton()
+    {
+
+    }
+
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        connectMenu.SetActive(false);
+        serverMenu.SetActive(false);
+    }
+}
